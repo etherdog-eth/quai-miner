025f580d6776410a835025f5a59709888460c716
wenjiping <7005763@qq.com>
2023-10-13 05:13:14 +0800

    quai挖矿安装程序

--- README.md
+++ README.md
@@ -3,12 +3,13 @@
 #### 安装
 
 ```commandline
-sudo apt update && sudo apt -y install wget && sudo wget https://raw.githubusercontent.com/wenjiping/quai/main/setup.sh && sudo chmod +x setup.sh && sudo ./setup.sh
+sudo apt update && sudo apt -y install wget 
+sudo wget https://raw.githubusercontent.com/wenjiping/quai-miner/main/setup.sh && sudo chmod +x setup.sh && sudo ./setup.sh
 ```
 
 #### 运行挖矿程序
 
 ```commandline
-sudo wget https://raw.githubusercontent.com/wenjiping/quai/main/miner.sh && sudo chmod +x miner.sh
+sudo wget https://raw.githubusercontent.com/wenjiping/quai-miner/main/miner.sh && sudo chmod +x miner.sh
 sudo ~/miner.sh
 ```
--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ git clone https://github.com/dominant-strategies/quai-gpu-miner && cd quai-gpu-m
 
 git submodule update --init --recursive
 
-sudo wget https://raw.githubusercontent.com/wenjiping/quai/main/CMakeLists.txt -O ~/quai-gpu-miner/libethash-cuda/CMakeLists.txt
-sudo wget https://raw.githubusercontent.com/wenjiping/quai/main/main.app -O ~/quai-gpu-miner/ethcoreminer/main.app
+sudo wget https://raw.githubusercontent.com/wenjiping/quai-miner/main/CMakeLists.txt -O ~/quai-gpu-miner/libethash-cuda/CMakeLists.txt
+sudo wget https://raw.githubusercontent.com/wenjiping/quai-miner/main/main.app -O ~/quai-gpu-miner/ethcoreminer/main.app
 
 mkdir build && cd build
 cmake .. && cmake --build .
